c719615d6b85ccf93d8ea242f919ffee007718b1
shemeshg <shemeshg@hotmail.com>
2024-10-24 19:41:43 +0300

    app Icon and readme

---
+++ SBOM.md
@@ -0,0 +1,7 @@
+# Built With
+
+- [Qt Quick and QT6 Open Source](https://www.qt.io/)
+- Icons made by Freepik from <www.flaticon.com>
+- Icons <https://fonts.google.com/icons>
+- [cog](https://nedbatchelder.com/code/cog)
+- [JSON for Modern C++](https://github.com/nlohmann/json)
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setOrganizationName("shemeshg");
+    //app.setOrganizationDomain("somecompany.com");
+    app.setApplicationName("Cakebrewjs2");
+
 
     QQmlApplicationEngine engine;
     QObject::connect(
