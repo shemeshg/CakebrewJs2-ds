b6f2e943caa1ef130bdd4212105aeff4e5ad44e8
shemeshg <gili.shemesh@gmail.com>
2026-01-14 09:27:30 +0200

    linux packaging

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include "config.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     app.setOrganizationName("shemeshg");
+    app.setApplicationVersion(PROJECT_VER);
     //app.setOrganizationDomain("somecompany.com");
     app.setApplicationName("Cakebrewjs2");
 
