eb42e74ac179af248031866aa33fe6a4fbb72cba
shemeshg <gili.shemesh@gmail.com>
2026-06-11 19:27:16 +0300

    101

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PROJECT_NAME "Cakebrewjs"
-#define PROJECT_VER  "2.100"
+#define PROJECT_VER  "2.101"
 #define PROJECT_VER_MAJOR "2"
-#define PROJECT_VER_MINOR "100"
+#define PROJECT_VER_MINOR "101"
 #define PTOJECT_VER_PATCH ""
